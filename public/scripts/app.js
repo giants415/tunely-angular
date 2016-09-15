e633cbc4db3992595a0fc3dac08bc90a76eda85c
Andrew <andrew.vinocur@gmail.com>
2016-09-15 11:15:04 -0700

    adds most recent changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,3 +3,28 @@
  * This is your main angular file. Edit as you see fit.
  *
  */
+
+console.log('app.js linked')
+
+angular
+  .module('tunely', [])
+  .controller('AlbumsIndexController', AlbumsIndexController);
+  function AlbumsIndexController () {
+    var vm = this;
+    vm.newAlbum = {};
+
+    vm.albums = [
+  {
+    name: 'Coming Home',
+    artistName: 'Leon Bridges'
+  },
+  {
+    name: 'Are We There',
+    artistName: 'Sharon Van Etten'
+  },
+  {
+    name: 'The Queen is Dead',
+    artistName: 'The Smiths'
+  }
+];
+  }
