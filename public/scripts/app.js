e94fd5441cd6c6decf7162b000e0bc8903f9c80e
Andrew <andrew.vinocur@gmail.com>
2016-09-15 13:22:59 -0700

    adds genres and functionality before POST step

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,7 +10,8 @@ angular
   // the second argument is a function that defines the capacities
   // of the controller.
 
-function AlbumsIndexController () {
+AlbumsIndexController.$inject = ['$http'];
+function AlbumsIndexController ($http) {
   var vm = this;
   vm.newAlbum = {};
 
@@ -19,18 +20,33 @@ function AlbumsIndexController () {
       artistName: 'Morrissey'
   };
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
+  vm.albums = [];
+
+  vm.getAlbum = function () {
+    $http({
+      method: 'GET',
+      url: '/api/albums'
+    }).then(function successCallback(response) {
+      vm.albums = response.data;
+    }, function errorCallback(response) {
+      console.log('There was an error getting the data', response);
+    });
+  };
+
+  vm.getAlbum();
+  
 }
+  // vm.albums = [
+  //   {
+  //     name: 'Coming Home',
+  //     artistName: 'Leon Bridges'
+  //   },
+  //   {
+  //     name: 'Are We There',
+  //     artistName: 'Sharon Van Etten'
+  //   },
+  //   {
+  //     name: 'The Queen is Dead',
+  //     artistName: 'The Smiths'
+  //   }
+  // ];
