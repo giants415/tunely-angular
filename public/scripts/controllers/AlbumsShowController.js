275b57faa3871d0adbb0550675a6fb2643f63aaf
Andrew <andrew.vinocur@gmail.com>
2016-09-19 11:36:59 -0700

    working on adding a new song, data doesnt show

--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -17,30 +17,40 @@ function AlbumsShowController ($http, $routeParams) {
     console.log('There was an error getting the data', response);
   });
 
-vm.deleteSong = function (song) {
-  // console.log('delete track', song);
-  $http({
-    method: 'DELETE',
-    url: '/api/albums/'+ $routeParams.id + '/songs/' + song._id
-      }).then(function successCallback(json) {
-        var index = vm.album.songs.indexOf(song);
-        vm.album.songs.splice(index, 1);
-      }, function errorCallback(response) {
-        console.log('Delete error', response);
-      });
-    }
+  vm.deleteSong = function (song) {
+    // console.log('delete track', song);
+    $http({
+      method: 'DELETE',
+      url: '/api/albums/'+ $routeParams.id + '/songs/' + song._id
+        }).then(function successCallback(json) {
+          var index = vm.album.songs.indexOf(song);
+          vm.album.songs.splice(index, 1);
+        }, function errorCallback(response) {
+          console.log('Delete error', response);
+        });
+      }
 
+  vm.editSong = function (song) {
+    $http({
+      method: 'PUT',
+      url: '/api/albums/'+ $routeParams.id + '/songs/' + song._id,
+      data: song
+    }).then(function editSuccess(json){
+      console.log('song edited');
+    });
+  }
 
-  // vm.addSong = function () {
-  //   $http({
-  //     method: 'POST',
-  //     url: 'api/albums',
-  //     data: vm.newTrack
-  //   }).then(function trackAddSuccess(response) {
-  //     vm.album.push(response.data);
-  //   }, function errorCallback(response) {
-  //     console.log('Error adding track', response);
-  //   });
-  // }
+  vm.addSong = function () {
+    $http({
+      method: 'POST',
+      url: 'api/albums/'+ $routeParams.id + '/songs/',
+      data: vm.newSong
+    }).then(function songAddSuccess(json) {
+      vm.album.songs.push(json.data);
+      // vm.newSong = {};
+    }, function errorCallback(response) {
+      console.log('Error adding track', response);
+    });
+  }
 
 }
